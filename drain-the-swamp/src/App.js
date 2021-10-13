ee2372084a15d12771c9397ef163edf575370559
John McQueen <john.mcqueen@wnco.com>
2021-10-13 10:47:11 -0500

    adding basic recoil setup

--- drain-the-swamp/src/App.js
+++ drain-the-swamp/src/App.js
@@ -1,13 +1,18 @@
+import { greenPointsState } from './selectors';
 import logo from './logo.svg';
 import './App.css';
+import { useRecoilValue } from 'recoil';
 
 function App() {
+  const greenPoints = useRecoilValue(greenPointsState);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>{greenPoints}</p>
         </p>
         <a
           className="App-link"
---
+++ drain-the-swamp/src/atoms.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const greenPoints = atom({
+    key: 'greenPoints',
+    default: 0
+});
