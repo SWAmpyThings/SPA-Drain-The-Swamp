90a4e4f3c3c40017c803c699b4e9e154c2260b95
John McQueen <john.mcqueen@wnco.com>
2021-10-14 08:05:16 -0500

    add basic myaccount styling

--- drain-the-swamp/src/individualDashboard/IndividualDashboard.js
+++ drain-the-swamp/src/individualDashboard/IndividualDashboard.js
@@ -5,7 +5,6 @@ import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import Header from '../shared/SouthwestHeader/index';
 import heartTree from './heart-tree.svg';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import { ProgressBar, Step } from 'react-step-progress-bar';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
@@ -25,15 +24,6 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 30,
-    borderRadius: 10,
-    [`&.${linearProgressClasses.determinate}`]: {
-        borderRadius: 20,
-        backgroundColor: '#E7E6E6'
-    }
-}));
-
 const GreenRewards = () => {
     const greenPoints = useRecoilValue(greenPointsState);
 
@@ -43,7 +33,7 @@ const GreenRewards = () => {
                 <Typography 
                     variant="h4"
                     align="left"
-                    sx={{ width: '100%', padding: '5px' }}>
+                    sx={{ fontWeight: 'bold', width: '100%', padding: '5px' }}>
                         Green Rewards
                 </Typography>
             </div>
@@ -200,68 +190,39 @@ const IndividualDashboard = (props) => {
     return (
         <div>
             <Header />
-            <Typography gutterBottom={true} variant="h2" align="left" sx={{ marginLeft: '5%' }}>
-                My Account
-            </Typography>
-            <Box sx={{ flexGrow: 1, margin: '5%' }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} lg={12}>
-                        <Item />
+            <div className="individual-dashboard--container">
+                <Typography gutterBottom={true} variant="h3" align="left" sx={{ color: '#FFFFFF', fontWeight: 'bold', marginLeft: '5%', paddingTop: '3%' }}>
+                    My Account
+                </Typography>
+                <Box sx={{ flexGrow: 1, margin: '0 5% 10%' }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} lg={12}>
+                            <Paper sx={{ height: 80, backgroundColor: '#304CB2', display: 'flex', alignItems: 'center'}}>
+                            <Typography gutterBottom={true} variant="h4" sx={{ color: '#FFBF27', marginLeft: '2%' }}>
+                            {/* Update with user that's logged in */}
+                                Hi, User!
+                            </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <Item />
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <Item />
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <Item />
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <Paper>
+                                <GreenRewards />
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} lg={6}>
-                        <Item />
-                    </Grid>
-                    <Grid item xs={12} lg={6}>
-                        <Item />
-                    </Grid>
-                    <Grid item xs={12} lg={6}>
-                        <Item />
-                    </Grid>
-                    <Grid item xs={12} lg={6}>
-                        <Paper>
-                            <GreenRewards />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Box>
+                </Box>
+            </div>
         </div>
     );
 };
 
 export default IndividualDashboard;
-
-
-
-// <div className="green-rewards-status">
-//                 <Typography gutterBottom={true} variant="h6">
-//                     Here is what you have earned towards badges
-//                 </Typography>
-//                 <div>
-//                     <Grid container>
-//                         <Grid item xs={12} md={10} lg={10}>
-//                             <BorderLinearProgress variant="determinate" value={80} />
-//                         </Grid>
-//                         <Grid item xs={1} md={1} lg={1}>
-//                             <img src={heartTree} className="green-rewards--icon" alt="heart tree" />
-//                         </Grid>
-//                     </Grid>
-//                     <Typography gutterBottom={true} variant="h7">
-//                         {/* insert points towards eco badge */}
-//                         1346 of 1500 for eco-badge
-//                     </Typography>
-//                 </div>
-//                 <div>
-//                     <Grid container>
-//                         <Grid item xs={12} md={10} lg={10}>
-//                             <BorderLinearProgress variant="determinate" value={62} />
-//                         </Grid>
-//                         <Grid item xs={1} md={1} lg={1}>
-//                             <img src={phonesHeart} className="green-rewards--icon" alt="phones heart" />
-//                         </Grid>
-//                     </Grid>
-//                     <Typography gutterBottom={true} variant="h7">
-//                         {/* insert points towards boarding badge */}
-//                         868 of 1500 for boarding badge
-//                     </Typography>
-//                 </div>
-//             </div>
